cfa717bbc3cb65001f28074a62f0b1863d11c2a1
John Brennan McAllister <johnb2221@gmail.com>
2025-05-29 14:21:15 -0500

    remove queue

--- src/Level2/selenium_version/bogo_bot_level2.py
+++ src/Level2/selenium_version/bogo_bot_level2.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import keyboard
-from collections import deque as queue
 
 load_dotenv()
 
